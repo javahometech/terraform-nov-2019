fb27c17b068ffea309bd668b5959415ea56a1f5a
Hari Kammana <hari.kammana@gmail.com>
2019-11-21 10:59:29 +0530

    terraform workspaces

--- provider.tf
+++ provider.tf
@@ -7,6 +7,7 @@ terraform {
     bucket = "in.javahome.930am-oct.tf"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "javahome-db"
   }
 }
 // Create VPC using terraform
