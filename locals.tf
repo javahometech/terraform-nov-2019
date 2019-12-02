001f29df52d78c2076cf2c9efe7e9dc9266cba29
Hari Kammana <hari.kammana@gmail.com>
2019-12-02 10:51:01 +0530

    modules

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  pri_sub_names = data.aws_availability_zones.azs.names
-  public_netnum = length(local.pri_sub_names)
-  pub_sub_ids   = aws_subnet.public.*.id
-  pri_sub_ids   = aws_subnet.private.*.id
-}
